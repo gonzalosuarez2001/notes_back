531600549bbce1302168ae20b006984d8d1088a5
gonzalo <gonzalosuarez9977@gmail.com>
2023-12-01 12:09:51 -0600

    segundo commit

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Importa el módulo Express
 const express = require('express');
 
+const cors = require('cors'); // Importa el paquete CORS
+
 // Crea una instancia de la aplicación Express
 const app = express();
+
+const corsOptions = {
+  origin: 'https://visionary-yeot-ac9a8c.netlify.app/', // Reemplaza con la URL de tu frontend en Netlify
+  optionsSuccessStatus: 200, // Algunos navegadores antiguos (IE11) interceptan las respuestas CORS con un código 204
+};
+
+app.use(cors(corsOptions));
 
 // Define un endpoint que devuelve "Hola, mundo!"
 app.get('/', (req, res) => {
